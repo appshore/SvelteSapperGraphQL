9b26180eac3d332a332188cb13238360ed1f50b2
AppShore <git@appshore.com>
2018-12-28 13:17:01 +0000

    review origin

--- src/config.js
+++ src/config.js
@@ -7,19 +7,18 @@ const config = {
   APP_NAME: '_APS',
   API_VERSION: 'api', // `api/${pjson.version.split('-')[0]}`,
   APP_VERSION: pjson.version,
-  BRAND: 'Svelte Web Solutions',
-  COMPANY: 'AppShore Inc.',
-  LOGO: 'AppShore.png',
-  WEBSITE: 'https://appshore.com',
+  BRAND: 'Svelte and Sapper',
+  COMPANY: 'SvelteSapper',
+  LOGO: 'SvelteSapper.png',
+  WEBSITE: 'https://sveltesapper.com',
   EMAIL: {
-    WEBMASTER: 'webmaster@appshore.com',
-    LEGAL: 'legal@appshore.com',
-    SALES: 'sales@appshore.com',
-    SUPPORT: 'support@appshore.com'
+    WEBMASTER: 'webmaster@sveltesapper.com',
+    LEGAL: 'legal@sveltesapper.com',
+    SALES: 'sales@sveltesapper.com',
+    SUPPORT: 'support@sveltesapper.com'
   },
   LICENSE: pjson.license,
   MOBILE_WIDTH: '960px', // upper limit to switch between mobile and web interfaces 
-  RECAPTCHA_KEY: '6Lf74QsTAAAAANwrE86qN9coHuyNsUnwowOFHmkt',
   AUTH_CHECK: 1000 * 60 * 60, // 1000 milliseconds * 60 seconds * 60 minutes = 1 hour
   COOKIE_TIMEOUT: 7, // 7 days
   DATA_SERVER: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api/2.1.0' : 'https://localhost:4000/api/2.1.0'
